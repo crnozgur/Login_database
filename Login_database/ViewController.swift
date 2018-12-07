be658592c17b101d3132051eedf5bc0d047f83f2
GO.DRE <go.dre@godres-macbook-pro.local>
2018-12-07 13:34:40 +0300

    Initial Commit

---
+++ Login_database/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login_database
+//
+//  Created by GO.DRE on 7.12.2018.
+//  Copyright © 2018 com.ceren.ozgur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
